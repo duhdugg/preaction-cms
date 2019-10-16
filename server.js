3a8fbceb8d2de25a7ca07dcf95a975a5e820bd8a
Doug Elkin <doug@dougelkin.com>
2019-10-16 11:12:43 -0400

    added commentary

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
 
+// <== LIBRARY IMPORTS ==>
+// bodyParser to handle json requests
 const bodyParser = require('body-parser')
+// cookieParser to handle cookies
 const cookieParser = require('cookie-parser')
+// express is the server-side framework of choice for this project
 const express = require('express')
+// path is needed to generate static build path later
 const path = require('path')
+// excerptHtml is used to generate descriptions from content
 const excerptHtml = require('excerpt-html')
+// sitemap is used to generate sitemaps
 const sm = require('sitemap')
 
+// app is our main express module
 const app = express()
+
+// <== LOCAL IMPORTS ==>
 const db = require('./lib/db.js')
 const settings = require('./lib/modules/settings.js')
 const pages = require('./lib/modules/pages.js')
@@ -15,20 +25,25 @@ const renderClient = require('./lib/render.js').renderClient
 const session = require('./lib/session.js')
 const uploads = require('./lib/modules/uploads.js')
 
+// <== http and socket.io setup ==>
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
+// <== express setup and module loading ==>
 app.use(settings.expressModule)
-
 app.use(cookieParser())
 app.use(session.session)
+// connect io to session
 io.use((socket, next) => {
   session.session(socket.request, socket.request.res, next)
 })
+// enable JSON requests and limit them to 50 megabytes in size
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(session.expressModule)
 app.use(pages.expressModule)
 app.use(uploads.expressModule)
+
+// <== ROUTES ==>
 
 app.route('/icon').get((req, res) => {
   db.model.Settings.findOne({ where: { key: 'icon' } }).then(setting => {
@@ -81,6 +96,7 @@ app.route('/sitemap.xml').get((req, res) => {
   })
 })
 
+// root route should generate description metadata from home page blocks
 app.route('/').get((req, res) => {
   pages.model.Page.findOne({
     where: { key: 'home' },
@@ -113,8 +129,11 @@ app.route('/').get((req, res) => {
   })
 })
 
+// root route should also serve the client build
 app.use('/', express.static(path.join(__dirname, 'build')))
 
+// all other routes should be caught here, served the appropriate page
+// description metadata generated from pageblocks
 app.route('*').get((req, res) => {
   let pageKey = req.path.split('/')[1]
   switch (pageKey) {
@@ -156,6 +175,8 @@ app.route('*').get((req, res) => {
   })
 })
 
+// <== SOCKET.IO EVENT CONFIG ==>
+
 io.on('connection', socket => {
   socket.on('save', fn => {
     fn()
@@ -170,6 +191,8 @@ io.on('connection', socket => {
     }
   })
 })
+
+// <== SERVER SETUP ==>
 
 const port = process.env.PREACTION_PORT || 8999
 
