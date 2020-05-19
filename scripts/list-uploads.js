457232441ba998e05f79fdfecec99a40dc045c9d
Doug Elkin <doug@dougelkin.com>
2020-05-19 09:34:35 -0400

    rewrote list-uploads script
    mainly to allow listing uploaded components,
    but also to use async/await and clean things up

--- scripts/list-uploads.js
+++ scripts/list-uploads.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const Op = require('sequelize').Op
 
 const db = require('../lib/db.js')
 const pages = require('../lib/modules/pages.js')
@@ -8,46 +9,41 @@ const ignore = ['.gitignore']
 
 const listUploads = () => {
   let uploads = fs.readdirSync(path.join(__dirname, '../uploads'))
-  uploads.forEach((filename) => {
+  uploads.forEach(async (filename) => {
     if (ignore.includes(filename)) {
       return
     }
-    let settingsCheck = db.model.Settings.findAll({
+    let inSetting = false
+    let inPageSetting = false
+    let inPageBlockImage = false
+    let inPageBlockComponent = false
+    let settingsWithFile = await db.model.Settings.findAll({
       where: { value: `uploads/${filename}` },
     })
-    let pageSettingsCheck = pages.model.Page.findAll({
+    let pagesWithBg = await pages.model.PageBlock.findAll({
       where: { settings: { '"bg"': `uploads/${filename}` } },
     })
-    let pageBlockImageCheck = pages.model.PageBlockContent.findAll({
+    let pageBlockImagesUsingFile = await pages.model.PageBlockContent.findAll({
+      where: { filename },
+    })
+    let pageBlockComponentsUsingFile = await pages.model.PageBlock.findAll({
       where: {
-        filename,
+        blockType: 'component',
+        settings: { '"src"': { [Op.like]: `%${filename}%` } },
       },
     })
-    Promise.all([settingsCheck, pageSettingsCheck, pageBlockImageCheck]).then(
-      () => {
-        let inSetting = false
-        let inPageSetting = false
-        let inPageBlockImage = false
-        settingsCheck.then((settings) => {
-          settings.forEach((setting) => {
-            inSetting = true
-          })
-          pageSettingsCheck.then((pages) => {
-            pages.forEach((page) => {
-              inPageSetting = true
-            })
-            pageBlockImageCheck.then((pageBlockContents) => {
-              pageBlockContents.forEach((pbc) => {
-                inPageBlockImage = true
-              })
-              if (inSetting || inPageSetting || inPageBlockImage) {
-                console.debug(`uploads/${filename}`)
-              }
-            })
-          })
-        })
-      }
-    )
+    inSetting = settingsWithFile.length > 0
+    inPageSetting = pagesWithBg.length > 0
+    inPageBlockImage = pageBlockImagesUsingFile.length > 0
+    inPageBlockComponent = pageBlockComponentsUsingFile.length > 0
+    if (
+      inSetting ||
+      inPageSetting ||
+      inPageBlockImage ||
+      inPageBlockComponent
+    ) {
+      console.log(`uploads/${filename}`)
+    }
   })
 }
 
