3f6ba40d3fea58ea481ed5e12e18c5bc10e86bee
Doug Elkin <doug@dougelkin.com>
2020-07-06 18:49:54 -0400

    improvements to testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
-/* global it */
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+test('renders without crashing', () => {
+  const app = render(<App />)
+  expect(app).toBeTruthy()
 })
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,3 @@
-/* global fetch */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
