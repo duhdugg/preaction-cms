74671c5c6d341d0036d9919f7b871df15618fd53
Doug Elkin <doug@dougelkin.com>
2020-08-12 23:24:44 -0400

    removed src/style from packaged contents

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,4 +2,4 @@
 
 rm preaction-cms.tar.gz && echo 'existing package deleted'
 uploads=$(node scripts/list-uploads.js)
-tar --exclude-vcs --exclude="src/style/index.template.js" -vcaf preaction-cms.tar.gz build data/db.sqlite src/style $uploads
+tar -vcaf preaction-cms.tar.gz build data/db.sqlite $uploads
