20634129bbd5cda2f973964d512e0e398706dbed
Doug Elkin <doug@dougelkin.com>
2021-06-28 16:00:45 -0400

    fix base styling

---
+++ src/style/base.js
@@ -0,0 +1,6 @@
+const ssr = typeof window === 'undefined'
+if (!ssr) {
+  require('bootstrap/dist/css/bootstrap.min.css')
+  require('react-quill/dist/quill.bubble.css')
+  require('react-quill/dist/quill.snow.css')
+}
