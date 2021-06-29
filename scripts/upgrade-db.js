91700a80b94cf1a99c445e8906e46c8b77c4c9e1
Doug Elkin <doug@dougelkin.com>
2021-06-29 17:07:00 -0400

    🗃️ handle db upgrades by tracking version in meta table

---
+++ scripts/upgrade-db.js
@@ -0,0 +1,67 @@
+const db = require('../lib/db.js')
+const pages = require('../lib/pages.js')
+const pkg = require('../package.json')
+
+db.sync().then(async () => {
+  const allMeta = await db.model.Meta.findAll()
+  let dbMeta
+  for (const meta of allMeta) {
+    if (meta.key === 'db') {
+      dbMeta = meta
+      break
+    }
+  }
+  if (dbMeta === undefined) {
+    dbMeta = await db.model.Meta.create({
+      key: 'db',
+      value: { version: '4.99' },
+    })
+    console.debug('upgrade-db', { dbVersion: dbMeta.value.version })
+  }
+  await db.model.User.findAll()
+  await db.model.Session.findAll()
+  const allPages = await pages.model.Page.findAll()
+  const updateMetaVersion = async () => {
+    dbMeta.value = { version: pkg.version }
+    await dbMeta.save()
+    console.log('upgrade-db', { upgradedTo: dbMeta.value.version })
+    return
+  }
+  if (dbMeta.value.version < '5.0') {
+    for (const page of allPages) {
+      page.settings.bodyGradient = false
+      page.settings.headerGradient = false
+      page.settings.jumboGradient = false
+      page.settings.mainGradient = false
+      page.settings.footerGradient = false
+      console.log('upgrade-db', `updating page id ${page.id}`)
+      page.settings = JSON.parse(JSON.stringify(page.settings))
+      await page.save()
+    }
+    const allPageBlocks = await pages.model.PageBlock.findAll()
+    for (const pageBlock of allPageBlocks) {
+      pageBlock.settings = Object.assign({}, pageBlock.settings, {
+        customClassName: '',
+        headerGradient: false,
+        bodyGradient: false,
+        // default xxlWidth to current lgWidth to preserve existing layout
+        xxlWidth: pageBlock.settings.lgWidth,
+      })
+      console.log('upgrade-db', `updating pageBlock id ${pageBlock.id}`)
+      await pageBlock.save()
+    }
+    const allContents = await pages.model.PageBlockContent.findAll()
+    for (const content of allContents) {
+      content.settings = Object.assign({}, content.setting, {
+        customClassName: '',
+        headerGradient: false,
+        bodyGradient: false,
+        // default xxlWidth to current lgWidth to preserve existing layout
+        xxlWidth: content.settings.lgWidth,
+      })
+      console.log('upgrade-db', `updating content id ${content.id}`)
+      await content.save()
+    }
+    await updateMetaVersion()
+  }
+})
