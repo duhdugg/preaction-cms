6e3d6714c4798b4fdd451d05e2960b8cf81a55a4
Doug Elkin <doug@dougelkin.com>
2021-06-30 16:01:14 -0400

    ⬆️ upgrade bs-clip dep which requires css import

--- src/style/base.js
+++ src/style/base.js
@@ -3,4 +3,5 @@ if (!ssr) {
   require('bootstrap/dist/css/bootstrap.min.css')
   require('react-quill/dist/quill.bubble.css')
   require('react-quill/dist/quill.snow.css')
+  require('@preaction/bootstrap-clips/dist/preaction-bootstrap-clips.css')
 }
